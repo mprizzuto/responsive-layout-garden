3d13e8d480fe674bdba4486b6a42ebb643633303
Marco R <mprizzuto88@gmail.com>
2022-05-26 15:53:26 -0400

    include html into section/inner-column

--- modules/garden-two.php
+++ modules/garden-two.php
@@ -1,86 +1,82 @@
-<section class="garden-two">
-  <inner-column>
-    <h2 class="heading-two" aria-hidden="true">FTV</h2>
-    
-    <div class="actress">
-      <section class="actress-info">
-        <h2 class="heading-two">adriana lima</h2>
-      
-      <h3 class="heading-three">le paris de</h3>
-      
-      <p class="model-desc">Adriana Francesca Lima is a Brazilian model and actress who is famous as the Victoria’s Secret Angel. Lima’s modeling career began at a young age in the elementary school, which proved to be a stepping stone in her modeling career. She was also the spokesmodel for Maybelline cosmetics from 2003 to 2009 and appeared in the company’s first calendar. She also did an ad-campaign for Swatch, which gave her additional recognition. Lima has appeared in several international editions of Vogue and Marie Claire. As a runway model, she has done the catwalks for designers like Giorgio Armani, Vera Wang and Valentino. Lima became a GUESS girl in 2000 and appeared in their fall ad campaign the very same year. In 2012, Lima earned the fourth position on the Forbes top-earning models list after she earned an estimated $7.3 million in one year.</p>
-      </section>
-      
-      <picture class="model-photo">
-        <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/restaurants/model-photo.jpg" alt="a model">
-      </picture>
-    </div>
-    
-    <div class="restaurants">
-      <h3>lets eat</h3>
-      <ul class="restaurant-list">
-       <li>
-         <restaurant-card>
-           <picture>
-             <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/restaurants/frank.jpg" alt="a restaurant">
-           </picture>
+<h2 class="heading-two" aria-hidden="true">FTV</h2>
 
-           <div class="restaurant-info">
-             <p class="restaurant-name">le frank</p>
+<div class="actress">
+  <section class="actress-info">
+    <h2 class="heading-two">adriana lima</h2>
+  
+  <h3 class="heading-three">le paris de</h3>
+  
+  <p class="model-desc">Adriana Francesca Lima is a Brazilian model and actress who is famous as the Victoria’s Secret Angel. Lima’s modeling career began at a young age in the elementary school, which proved to be a stepping stone in her modeling career. She was also the spokesmodel for Maybelline cosmetics from 2003 to 2009 and appeared in the company’s first calendar. She also did an ad-campaign for Swatch, which gave her additional recognition. Lima has appeared in several international editions of Vogue and Marie Claire. As a runway model, she has done the catwalks for designers like Giorgio Armani, Vera Wang and Valentino. Lima became a GUESS girl in 2000 and appeared in their fall ad campaign the very same year. In 2012, Lima earned the fourth position on the Forbes top-earning models list after she earned an estimated $7.3 million in one year.</p>
+  </section>
+  
+  <picture class="model-photo">
+    <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/restaurants/model-photo.jpg" alt="a model">
+  </picture>
+</div>
 
-             <p class="restaurant-desc">Le Frank - Fondation Louis Vuitton is pretty near Fondation Louis Vuitton. If you never appeared to try French cuisine, take your chance at this restaurant. Perfectly cooked fried salmon, mascarpone and smoked ribs are the best dishes. Many guests come to enjoy good berry dessert, strawberry dessert and lemon dessert. At Le Frank - Fondation Louis Vuitton, visitors can drink delicious wine, champagne or white wine. Great lemon juice, hot chocolate or lemonade will make your meal even more delicious and you'll surely come back. Choose between indoor and outdoor seating.</p>
+<div class="restaurants">
+  <h3>lets eat</h3>
+  <ul class="restaurant-list">
+   <li>
+     <restaurant-card>
+       <picture>
+         <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/restaurants/frank.jpg" alt="a restaurant">
+       </picture>
 
-             <address>
-               <p>8 avenue du Mahatma Gandhi</p>
-               <p><a href="tel:+33158442570" target="_external">le frank</a></p>
-               <a href="http://www.restaurantlefrank.fr/?lang=en" target="_external">restaurantlefrank.fr</a>
-             </address>
-           </div>
-         </restaurant-card> 
-       </li>
+       <div class="restaurant-info">
+         <p class="restaurant-name">le frank</p>
 
-        <li>
-         <restaurant-card>
-           <picture>
-             <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/restaurants/maurice.jpg" alt="a restaurant">
-           </picture>
-           
-           <div class="restaurant-info">
+         <p class="restaurant-desc">Le Frank - Fondation Louis Vuitton is pretty near Fondation Louis Vuitton. If you never appeared to try French cuisine, take your chance at this restaurant. Perfectly cooked fried salmon, mascarpone and smoked ribs are the best dishes. Many guests come to enjoy good berry dessert, strawberry dessert and lemon dessert. At Le Frank - Fondation Louis Vuitton, visitors can drink delicious wine, champagne or white wine. Great lemon juice, hot chocolate or lemonade will make your meal even more delicious and you'll surely come back. Choose between indoor and outdoor seating.</p>
 
-            <p class="restaurant-name">le hotel maurice</p>
+         <address>
+           <p>8 avenue du Mahatma Gandhi</p>
+           <p><a href="tel:+33158442570" target="_external">le frank</a></p>
+           <a href="http://www.restaurantlefrank.fr/?lang=en" target="_external">restaurantlefrank.fr</a>
+         </address>
+       </div>
+     </restaurant-card> 
+   </li>
 
-            <p class="restaurant-desc">In this charming boutique hotel in Nice center, come and enjoy a restful and peaceful stay in an establishment of character combining Art Deco and contemporary spirit. Book one of our 23 comfortable and equipped rooms, just a few steps from the Mediterranean Sea and the Promenade des Anglais. In this refined and warm atmosphere, stay at the Meurice, a hotel in Nice center, with its exceptional geographical location and 3-star services.</p>
+    <li>
+     <restaurant-card>
+       <picture>
+         <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/restaurants/maurice.jpg" alt="a restaurant">
+       </picture>
+       
+       <div class="restaurant-info">
 
-            <address>
-              <p>14 avenue de Suède, 06000 Nice</p>
-              <p><a href="tel:0497030520" target="_external">maurice</a></p>
-              <a href="https://www.hotel-le-meurice.com/en" target="_external">www.hotel-le-meurice.com</a>
-            </address>
-          </div>
-         </restaurant-card> 
-       </li>
+        <p class="restaurant-name">le hotel maurice</p>
 
-        <li>
-         <restaurant-card>
-           <picture>
-             <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/restaurants/galignani.jpg" alt="a restaurant">
-           </picture>
-           
-           <div class="restaurant-info">
+        <p class="restaurant-desc">In this charming boutique hotel in Nice center, come and enjoy a restful and peaceful stay in an establishment of character combining Art Deco and contemporary spirit. Book one of our 23 comfortable and equipped rooms, just a few steps from the Mediterranean Sea and the Promenade des Anglais. In this refined and warm atmosphere, stay at the Meurice, a hotel in Nice center, with its exceptional geographical location and 3-star services.</p>
 
-            <p class="restaurant-name">galignani</p>
+        <address>
+          <p>14 avenue de Suède, 06000 Nice</p>
+          <p><a href="tel:0497030520" target="_external">maurice</a></p>
+          <a href="https://www.hotel-le-meurice.com/en" target="_external">www.hotel-le-meurice.com</a>
+        </address>
+      </div>
+     </restaurant-card> 
+   </li>
 
-            <p class="restaurant-desc">Librairie Galignani is refined and elegant, it has a feel as distinguished as its location on Rue de Rivoli. The last time I was there I spotted Karl Lagerfeldt, one of the stores many loyal customers (Galignani is where he finds art books for inspiration). The staff is so polite and correct that you understand that this is a store that many ...</p>
+    <li>
+     <restaurant-card>
+       <picture>
+         <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/restaurants/galignani.jpg" alt="a restaurant">
+       </picture>
+       
+       <div class="restaurant-info">
 
-             <address>
-               <p>224 rue de Rivoli, 75001 Paris France</p>
-               <p><a href="tel:0142607607" target="_external">librari galignani</a></p>
-               <a href="https://www.galignani.fr/" target="_external">galignani</a>
-             </address>
-           </div>
-         </restaurant-card> 
-       </li> 
-      </ul>
-    </div>
-  </inner-column>
-</section>
+        <p class="restaurant-name">galignani</p>
+
+        <p class="restaurant-desc">Librairie Galignani is refined and elegant, it has a feel as distinguished as its location on Rue de Rivoli. The last time I was there I spotted Karl Lagerfeldt, one of the stores many loyal customers (Galignani is where he finds art books for inspiration). The staff is so polite and correct that you understand that this is a store that many ...</p>
+
+         <address>
+           <p>224 rue de Rivoli, 75001 Paris France</p>
+           <p><a href="tel:0142607607" target="_external">librari galignani</a></p>
+           <a href="https://www.galignani.fr/" target="_external">galignani</a>
+         </address>
+       </div>
+     </restaurant-card> 
+   </li> 
+  </ul>
+</div>
