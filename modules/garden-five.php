b259db439fcf1d3b987daf11ca32f61dd934fa78
Marco R <mprizzuto88@gmail.com>
2022-05-27 22:26:22 -0400

    replace double w/ single quotes. html for garden 6

---
+++ modules/garden-five.php
@@ -0,0 +1,54 @@
+<chapter-card>
+  <picture class="produce">
+    <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/juices/spinach.jpg" alt="a bowl of raw spinach">
+  </picture>
+
+  <div class="chapter-info">
+    <h2>Introduction</h2>
+
+    <ul class="chapter-list">
+     <li>
+       <a href="#">why go green?</a>
+     </li>
+     <li>
+       <a href="#">the greens</a>
+     </li>
+     <li>
+       <a href="#">adding fruit</a>
+     </li>
+     <li>
+       <a href="#">adding superfoods</a>
+     </li>
+     <li>
+       <a href="#">sweetners(if you must)</a>
+     </li>
+     <li>
+       <a href="#">Tips, buying, aving, growing, and storing</a>
+     </li>
+    </ul>
+  </div>
+</chapter-card>
+
+<chapter-card>
+  <picture class="produce">
+    <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/juices/berries.jpg" alt="raspberries">
+  </picture>
+
+  <div class="chapter-info">
+    <h2>The green drink diet</h2>
+
+    <ul class="chapter-list">
+     <li>
+       <a href="#">Think supplement not diet</a>
+     </li>
+     <li>
+       <a href="#">Making your green drink:,
+Equiptment and how to use it</a>
+     </li>
+     <li>
+       <a href="#">Juicing vs blending</a>
+     </li>
+
+    </ul>
+  </div>
+</chapter-card>
