557c7ec235b8b2a264049558200773a483b6d677
Marco R <mprizzuto88@gmail.com>
2022-06-06 23:14:19 -0400

    create HTML for garden seven

---
+++ modules/garden-seven.php
@@ -0,0 +1,78 @@
+<fashion-editorial>
+  <header class="fashion-header">
+    <inner-column>
+      <nav class="fashion-nav">
+        <a href="">editorial</a>
+        
+        <ul class="social-links">
+          <li>
+            <a href="#">facebook</a>
+          </li>
+          <li>
+            <a href="#">twitter</a>
+          </li>
+          <li>
+            <a href="#">instagram</a>
+          </li>
+        </ul>
+        
+        <ul class="icon-list">
+          <li title="click if you're happy">
+            <a href="">:)</a>
+          </li>
+          <li title="click if you're angry">
+            <a href="">>:(</a>
+          </li>
+          <li title="click if you're excited">
+            <a href="">:!</a>
+          </li>
+          <li title="click if you're apathetic">
+            <a href="">:|</a>
+          </li>
+          <li title="click if you have a mustache">
+            <a href="">:{</a>
+          </li>
+        </ul>
+      </nav>
+    </inner-column>
+  </header>
+  
+  <section class="fashion-mode">
+    <inner-column>
+      <h3>mode</h3>
+      
+      <div class="better-mode">
+        <p>turn on the better mode and discuss new collections of fashion multiples- studio</p>
+        <svg class="fashion-icon" width="100%" height="100%" viewBox="0 0 201 193" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+            <g transform="matrix(1,0,0,1,-288.595,-139.288)">
+                <g transform="matrix(1,0,0,1,87.2464,-86.0284)">
+                    <path d="M311.218,225.767C304.774,225.166 298.285,225.166 291.841,225.767L295.089,281.071C292.001,281.525 288.974,282.303 286.063,283.392L260.065,233.939C254.171,236.511 248.551,239.625 243.283,243.238L274.908,289.573C272.469,291.448 270.254,293.575 268.3,295.915L220.021,265.563C216.257,270.619 213.012,276.013 210.332,281.669L261.86,306.621C260.725,309.415 259.914,312.32 259.441,315.284L201.818,312.166C201.192,318.351 201.192,324.58 201.818,330.764L259.441,327.647C259.914,330.611 260.725,333.516 261.86,336.309L210.332,361.261C213.012,366.918 216.257,372.312 220.021,377.367L268.3,347.016C270.254,349.356 272.469,351.482 274.908,353.357L243.283,399.693C248.551,403.306 254.171,406.42 260.065,408.992L286.063,359.538C288.974,360.628 292.001,361.406 295.089,361.86L291.841,417.163C298.285,417.764 304.774,417.764 311.218,417.163L307.97,361.86C311.058,361.406 314.085,360.628 316.996,359.538L342.994,408.992C348.888,406.42 354.508,403.306 359.776,399.693L328.151,353.357C330.59,351.482 332.805,349.356 334.759,347.016L383.038,377.367C386.802,372.312 390.047,366.918 392.727,361.261L341.199,336.309C342.334,333.516 343.145,330.611 343.618,327.647L401.241,330.764C401.867,324.58 401.867,318.351 401.241,312.166L343.618,315.284C343.145,312.32 342.334,309.415 341.199,306.621L392.727,281.669C390.047,276.013 386.802,270.619 383.038,265.563L334.759,295.915C332.805,293.575 330.59,291.448 328.151,289.573L359.776,243.238C354.508,239.625 348.888,236.511 342.994,233.939L316.996,283.392C314.085,282.303 311.058,281.525 307.97,281.071L311.218,225.767ZM301.53,302.236C312.588,302.236 321.566,310.852 321.566,321.465C321.566,332.079 312.588,340.695 301.53,340.695C290.471,340.695 281.493,332.079 281.493,321.465C281.493,310.852 290.471,302.236 301.53,302.236Z" style="fill:rgb(173,0,0);"/>
+                </g>
+            </g>
+        </svg>
+      </div>
+    </inner-column>
+  </section>
+  
+  <section class="studio-offer">
+    <inner-column>
+      <div class="studio-shot">
+        <h4>01</h4>
+        
+        <p>photo shoot in a film studio made on behalf of your client. I invite you to see the new collection</p>
+      </div>
+      
+      <div class="studio-approach">
+        <h4>&copy; 2021</h4>
+        <p>a stylish approach to presenting fashion and creating the right atmosphere. We are famous for a beautiful clothing style</p>
+      </div>
+    </inner-column>
+  </section>
+  
+  <figure class="model">
+    <picture class="fashion-model">
+      <img src="https://fashionweekdaily.com/wp-content/uploads/2015/09/20150228_PUCCI_0068.jpg" alt="taylor hill">
+    </picture>
+    <figcaption>Model <em>taylor hill</em> in our newest fashion</figcaption>
+  </figure>
+</fashion-editorial>
