df219549d6052fc90820157e385e8a4f1f6355dd
Marco R <mprizzuto88@gmail.com>
2022-06-06 23:51:28 -0400

    add html for garden-eight.php

---
+++ modules/garden-eight.php
@@ -0,0 +1,68 @@
+<rap-album>
+  <h3>n.w.a</h3>
+  
+  <h4>greatest hits</h4>
+  
+  <ul class="rapper-list">
+    <li>
+      <figure>
+        <picture>
+          <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/nwa/ice-cube.jpg" alt="ice cube">
+        </picture>
+        
+        <figcaption>ice cube</figcaption>
+      </figure>
+    </li>
+    
+    <li>
+      <figure>
+        <picture>
+          <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/nwa/mc-ren.jpg" alt="mc ren">
+        </picture>
+        
+        <figcaption>mc ren</figcaption>
+      </figure>
+    </li>
+    
+    <li>
+      <figure>
+        <picture>
+          <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/nwa/eazy-e.jpg" alt="eazy e">
+        </picture>
+        
+        <figcaption>eazy e</figcaption>
+      </figure>
+    </li>
+    
+    <li>
+      <figure>
+        <picture>
+          <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/nwa/yella.jpg" alt="yella">
+        </picture>
+        
+        <figcaption>yella</figcaption>
+      </figure>
+    </li>
+    
+    <li>
+      <figure>
+        <picture>
+          <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/nwa/dr-dre.jpg" alt="dr dre">
+        </picture>
+        
+        <figcaption>dr dre</figcaption>
+      </figure>
+    </li>
+  </ul>
+  
+  <div class="nwa-rep">
+    <p class="nwa-claim">
+      <span class="nwa-color">the worlds most dangerous group</span>
+      <span class="nwa-color">includes previously unreleased material</span>
+    </p>
+    
+    <picture class="parent-warning">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Parental_Advisory_label.svg/1200px-Parental_Advisory_label.svg.png" alt="parental advisory">
+    </picture>
+  </div>
+</rap-album>
