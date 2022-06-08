d2492296ec2db8ae7e713ef0ded7832f067a26b4
Marco R <mprizzuto88@gmail.com>
2022-06-07 23:36:26 -0400

    add html fpr garden-13. include in index.php

---
+++ modules/garden-thirteen.php
@@ -0,0 +1,33 @@
+<author-card>
+  <header class="author-card-header">
+    <p class="estimated-time"><em>9 minute read</em></p>
+  </header>
+  
+  <section class="attachment-style">
+    <div class="fearful">
+      <h3>your guide to fearful avoidant attachment: How to spot it, signs you've got it, & how to fix it</h3>
+      
+      <p class="guide">the guide you need for your relationships</p>
+    </div>
+    
+    <aside>
+      <h4><a href="#">crystaljackson.medium.com</a></h4>
+    </aside>
+  </section>
+  
+  <footer class="author-card-footer">
+    <div class="author-branding">
+      <figure class="author-figure">
+        <picture class="crystal-photo">
+          <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/crystal-jackson.jpg" alt="">
+        </picture>
+      
+        <figcaption class="author-name">crystal jackson</figcaption>
+      </figure>
+      
+      <picture class="medium-logo" aria-hidden="true">
+        <img src="https://cutewallpaper.org/24/medium-logo-png/medium-db3b2-logo-2cbe7-usage-e83fc-guidelines-f9f1e-by-ae9c6-medium-13602-mediumdesign.png" alt="medium logo">
+      </picture>
+    </div>
+  </footer>
+</author-card>
