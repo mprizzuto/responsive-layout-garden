7dbb9c3c31609541040b18af8aac1d98e55dfc0a
Marco R <mprizzuto88@gmail.com>
2022-06-12 18:08:18 -0400

    include garden 19

---
+++ modules/garden-nineteen.php
@@ -0,0 +1,33 @@
+<live-event>
+  <picture class="be-you">
+    <img src="https://bloximages.chicago2.vip.townnews.com/primepublishers.com/content/tncms/assets/v3/editorial/9/4e/94e09384-a3fd-5118-9c83-74e53ea96fec/627a6c86d19f8.image.jpg?crop=1790%2C940%2C0%2C109&resize=1200%2C630&order=crop%2Cresize" alt="vetinary hospitals, unapologetically you">
+  </picture>
+
+  <div class="guest-speakers">
+    <picture class="guest-speaker">
+      <img src="https://peprojects.dev/images/square.jpg" alt="">
+    </picture>
+
+    <picture class="guest-speaker">
+      <img src="https://peprojects.dev/images/square.jpg" alt="">
+    </picture>
+
+    <picture class="guest-speaker">
+      <img src="https://peprojects.dev/images/square.jpg" alt="">
+    </picture>
+
+    <picture class="guest-speaker">
+      <img src="https://peprojects.dev/images/square.jpg" alt="">
+    </picture>
+
+    <picture class="guest-speaker">
+      <img src="https://peprojects.dev/images/square.jpg" alt="">
+    </picture>
+  </div>
+   
+  <div class="insta-event">
+    <h4 class="join-us">join us for our next <span class="live-event">instagram live event</span></h4>
+   
+    <h4 class="at-work"><q>being unapologetically you at work</q> <span class="live-event">wednesday, june 8 | 1pm PST</span></h4>
+  </div>
+</live-event>
