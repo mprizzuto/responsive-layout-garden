7ec9a91253e0ecd8b282fbd3eb5cd48b52690e35
Marco R <mprizzuto88@gmail.com>
2022-06-12 19:53:46 -0400

    add html for garden 22

---
+++ modules/garden-twenty-two.php
@@ -0,0 +1,18 @@
+<art-fair>
+  <h3>the</h3>
+  <h3>other</h3>
+  <h3>art</h3>
+  <h3>fair</h3>
+  
+  <art-wall>
+    <art></art>
+    <art></art>
+    <art></art>
+    <art></art>
+    <art></art>
+    <art></art>
+    <art></art>
+    <art></art>
+    <art></art>
+  </art-wall>
+</art-fair>
