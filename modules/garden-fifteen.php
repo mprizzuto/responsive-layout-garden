ba311cebf580f80748886b67c530d2f3ba67a5a2
Marco R <mprizzuto88@gmail.com>
2022-06-12 16:26:00 -0400

    add html for garden-15. include in index.php

---
+++ modules/garden-fifteen.php
@@ -0,0 +1,35 @@
+<snapshots>
+  <h3 class="heading-three">MR photos</h3>
+  
+  <ul class="picture-list">
+    <li>
+      <picture class="snapshot-two">
+        <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/photo-grid-two/lamp.jpg" alt="a red lamp">
+      </picture>
+    </li>
+
+     <li>
+      <picture class="snapshot-two">
+        <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/photo-grid-two/lake.jpg" alt="a lake with trees">
+      </picture>
+    </li>
+
+     <li>
+      <picture class="snapshot-two">
+        <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/photo-grid-two/flower-two.jpg" alt="a yellow flower">
+      </picture>
+    </li>
+
+     <li>
+      <picture class="snapshot-two">
+        <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/photo-grid-two/bottle.jpg" alt="">
+      </picture>
+    </li>
+
+     <li>
+      <picture class="snapshot-two">
+        <img src="https://peprojects.dev/alpha-1/mprizzuto/images/rlg/photo-grid-two/selection-one.jpg" alt="pink flowers">
+      </picture>
+    </li>
+  </ul>
+</snapshots>
