d32f95156cf3598b0e69b5762d79c46b0c50049b
Marco R <mprizzuto88@gmail.com>
2022-06-12 23:34:18 -0400

    add HTML for garden 27

---
+++ modules/garden-twenty-seven.php
@@ -0,0 +1,20 @@
+<privacy-poster>
+  <pink-card>
+  
+  </pink-card>
+  <p class="client-cta">start taking back your personal information start taking back your personal</p>
+  
+  <p class="client-cta">start taking back your personal information start taking back your personal</p>
+  
+  <p class="client-cta">start taking back your personal information start taking back your personal</p>
+  
+  <privacy-card>
+    <ul class="privacy-mantra">
+      <li>privacy <span class="mantra-color">is</span> <span class="your-right mantra-color">your right</span></li>
+      <li>privacy <span class="mantra-color">is</span> <span class="your-right mantra-color">your right</span></li>
+      <li>privacy <span class="mantra-color">is</span> <span class="your-right mantra-color">your right</span></li>
+      </ul>
+      <p class="dv-message">For survivors of domestic violence, harassment, and stalking, having your personal details on the internet is not just annoying and intrusive, <strong>it's dangerous.</strong></p>
+  </privacy-card>
+  <p class="client-cta">start taking back your personal information start taking back your personal</p>
+</privacy-poster>
