20f6999ec5b19497b86c8a4f2656908913414b53
Marco R <mprizzuto88@gmail.com>
2022-06-12 22:33:08 -0400

    add html for garden 23

---
+++ modules/garden-twenty-three.php
@@ -0,0 +1,17 @@
+<daily-affirmations>
+  <header class="affirmation-header">
+    <h3>morning affirmations</h3>
+
+    <ul class="affirmations">
+      <li>I will have a peaceful way</li>
+      <li>i deserve to rest</li>
+      <li>i will make progress</li>
+      <li>i am sytronger than yesterday</li>
+      <li>i am worthy</li>
+      <li>i am open to healing</li>
+      <li>i am prepared fully for the day</li>
+      <li>i choose to be happy</li>
+      <li>i will love myself and others</li>
+    </ul>
+  </header>
+</daily-affirmations>
