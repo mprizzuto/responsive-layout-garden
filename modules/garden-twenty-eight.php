c6b3474eb767b0a3d31c8ad7698c08fc22720d08
Marco R <mprizzuto88@gmail.com>
2022-06-12 23:50:10 -0400

    add HTML for garden-28

---
+++ modules/garden-twenty-eight.php
@@ -0,0 +1,31 @@
+<derm-card>
+  <table>
+    <tbody>
+      <tr>
+        <td>
+          <picture class="doc-portrait">
+            <img src="https://starrfm.com.gh/wp-content/uploads/2021/05/h-696x929.jpeg" alt="a woman with long hair smiles">
+          </picture>
+        </td>
+        <td class="doc-branding"><span class="derm-name">kavita mariwalla, MD</span>
+          <picture class="derm-logo">
+            <img src="https://scienceofskincaresummit.com/wp-content/uploads/2021/05/88.png" alt="">
+          </picture>
+        </td>
+      </tr>
+      
+      <tr>
+        <td></td>
+        <td>18 years</td>
+      </tr>
+      
+      <tr>
+        <td></td>
+        <td>board-certified dermatologist speaker</td>
+      </tr>
+    </tbody>
+  </table>
+  
+  <p class="derm-summit">SCIENCE OF SKINCARE SUMMIT 2022 <span class="austin-tx">austin, tx | oct 28-30</span></p>
+
+</derm-card>
