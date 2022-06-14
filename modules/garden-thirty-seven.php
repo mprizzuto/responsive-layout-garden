0a9d9c7c6def32298f1e8a16fa607d4d7d17d0bf
Marco R <mprizzuto88@gmail.com>
2022-06-14 00:04:11 -0400

    add/include HTML for garden 37

---
+++ modules/garden-thirty-seven.php
@@ -0,0 +1,11 @@
+<advertisements>
+  <ul class="ad-list">
+    <li class="ad-one" aria-description="bananas"><span>bananas, the super fruit?</span></li>
+    <li class="ad-two" aria-description="raspberries"><span class="play-button">
+  &#9654;</span> get fruit</li>
+    <li class="ad-three" aria-description="grapes"><span>grapes</span></li>
+    <li class="ad-four" aria-description="apples"><span class="play-button">
+  &#9654;</span> eat fruit</li>
+  <!--   <li class="ad-five"><span>mangoes</span></li> -->
+  </ul>
+</advertisements>
