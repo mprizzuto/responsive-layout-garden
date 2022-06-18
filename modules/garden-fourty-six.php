8f2d4434079683c3f854ea92f9d58eb522f7b379
Marco R <mprizzuto88@gmail.com>
2022-06-18 17:18:27 -0400

    add/include HTML for garden 46

---
+++ modules/garden-fourty-six.php
@@ -0,0 +1,58 @@
+<health-influencer>
+  <h3>just a few of the topics</h3>
+
+  <ul class="influencer-list">
+    <li>
+      <influencer-card>
+        <figure>
+          <picture>
+            <img src="https://images.squarespace-cdn.com/content/v1/5de5bdd50b9c8e17ffb33426/1622813186315-TH6L0JGAIXDHT35KQ1M6/pic5.jpeg" alt="a blonde woman smiles in front of fresh produce">
+          </picture>
+
+          <figcaption>@organic_blondie</figcaption>
+        </figure>
+
+        <ul class="health-services">
+          <li>nutrional cleansing</li>
+          <li>with essential oils</li>
+          <li>courtney bursich</li>
+          <li>functional diagnostic</li>
+          <li>nutrition practioner</li>
+        </ul>
+      </influencer-card>
+    </li>
+
+    <li>
+      <influencer-card>
+        <figure>
+          <picture>
+            <img src="https://static.showit.co/1200/MeHY_dDpS7yIdNOHk7gDFA/68040/jpeg_image-2dec7af7e7f6-1.jpg" alt="a blonde woman in a white coat smiles">
+          </picture>
+
+          <figcaption>@the_acne_np</figcaption>
+        </figure>
+
+        <ul class="health-services">
+          <li>healthy skin and essntial oils</li>
+          <li>kristin cunningham <span class="healthcare-title">the acne np</span></li>
+        </ul>
+      </influencer-card>
+    </li>
+
+    <li>
+      <influencer-card>
+        <figure>
+          <picture>
+            <img src="https://images.squarespace-cdn.com/content/v1/5e421d281a74c5028d177d16/1608252642548-WGU3CKQWHDADJ2F9MIXF/Natalie+headshot+white+coat+1.JPG" alt="a blonde woman in a white coat smiles">
+          </picture>
+
+          <figcaption>@the.pcos.doc</figcaption>
+        </figure>
+
+        <ul class="health-services">
+          <li>womens health & hormones using essential oils: Dr natalie underberg <span class="healthcare-title">the PCOS doc</span></li>
+        </ul>
+      </influencer-card>
+    </li>
+  </ul>
+</health-influencer>
