5f7ded4adfec03ad5a6b811b593c4e070e1191a4
Marco R <mprizzuto88@gmail.com>
2022-06-18 19:11:39 -0400

    add HTML for garden 48

---
+++ modules/garden-fourty-eight.php
@@ -0,0 +1,43 @@
+<typographer>
+	<header class="typographer-header">
+		<h3 class="typographer-name"><span class="name">jan tschichold</span></h3>
+
+		<h3 class="typographer-role"><span class="role">titan of typography</span></h3>
+	</header>
+
+	<section class="typographer-rep">
+		<blockquote class="type-advocacy" cite="https://en.wikipedia.org/wiki/Jan_Tschichold">
+			<p><span class="leading-advocate">leading advocate of</span> <strong>modernist design in the late twenties</strong></p>
+		</blockquote>
+	</section>
+
+	<aside class="typographer-info">
+		<figure class="typographer">
+			<picture class="typographer-portrait">
+				<img src="https://i.ibb.co/nD9f6B5/tshci.png">
+			</picture>
+
+			<figcaption class="jan-rep">
+				<span class="prominent-type">prominent twentieth century german typographer </span>
+
+				<svg class="ballpoint" width="100%" height="100%" viewBox="0 0 200 200" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+				    <g transform="matrix(1,0,0,1,-118.847,-90.6939)">
+				        <g transform="matrix(1.02923,0,0,1.13545,39.1367,-206.083)">
+				            <ellipse cx="174.606" cy="349.445" rx="97.16" ry="88.071" style="fill:rgb(236,236,236);"/>
+				        </g>
+				    </g>
+				</svg>
+
+				<span class="book-designer">book designer</span>
+
+				<svg class="pen-handle" width="100%" height="100%" viewBox="0 0 600 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+				    <g transform="matrix(1,0,0,1,-41.2348,-437.138)">
+				        <g transform="matrix(1.07346,0,0,0.490728,-3.02892,250.126)">
+				            <rect x="41.235" y="381.09" width="558.943" height="101.889" style="fill:rgb(236,236,236);"/>
+				        </g>
+				    </g>
+				</svg>
+			</figcaption>
+		</figure>
+	</aside>
+</typographer>
