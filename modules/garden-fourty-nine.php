cd3f87894e21d62435cb79bc3dc57610462f9d30
Marco R <mprizzuto88@gmail.com>
2022-06-19 22:26:15 -0400

    include html for garden 49

--- modules/garden-fourty-nine.php
+++ modules/garden-fourty-nine.php
@@ -2,12 +2,21 @@
 	<header class="design-school-header">
 		<h3 class="pe-sign">
 			<span class="school-name">perpetual education</span>
-			<span class="course-name">Design for the web</span>
+			<span class="course-name">Design for the web <abbr class="course-abbr" title="design for the web">(dftw)</abbr></span>
 		</h3>
 
 		<h3 class="pe-cta-title">
 			<span class="pe-cta">learn <em>web design</em></span>
 			<span class="pe-cta">and programming</span>
 		</h3>
+
+		<svg class="pe-dot" width="100%" height="100%" viewBox="0 0 300 300" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+		    <g transform="matrix(1,0,0,1,-267.075,-99.0311)">
+		        <g transform="matrix(0.830744,0,0,0.776287,165.538,-24.5497)">
+		            <ellipse cx="302.785" cy="352.422" rx="180.561" ry="193.227" style="fill:rgb(132,0,0);"/>
+		        </g>
+		    </g>
+		</svg>
+
 	</header>
 </pe-teaser>
