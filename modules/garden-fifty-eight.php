baaaeb2e2f9ba224352d354e7f78db94fa5ac594
Marco R <mprizzuto88@gmail.com>
2022-06-23 17:18:46 -0400

    write html for garden 58

--- modules/garden-fifty-eight.php
+++ modules/garden-fifty-eight.php
@@ -1,1 +1,21 @@
-garden-fifty-eight.php
+<realtor-poster>
+	<header class="realtor-poster-header">
+		<h3>beyond units & volume</h3>
+	</header>
+
+	<picture class="realtor-couple">
+		<img src="">
+	</picture>
+
+	<div class="realtor-rep">
+		<h4>the pasillas team</h4>
+
+		<p>Loan Consultants <span class="license-number">NMLS#269718 & 204637</span></p>
+	</div>
+
+	<h4 class="business-year">2021</h4>
+
+	<picture class="realtor-logo">
+		<img src="ic.com/images?q=tbn:ANd9GcS5NS1NEgFttmYfKoRkbS8F6ygCb26FP_e8ByENYdMKIgax8DSONsnoypkQrOaytUHLeAo&usqp=CAU" alt="new american funding">
+	</picture>
+</realtor-poster>
