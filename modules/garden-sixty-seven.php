16b1a0d02feb79091cdc1c8c0eacee8066663d23
Marco R <mprizzuto88@gmail.com>
2022-06-27 22:50:43 -0400

    create garden-sixty-seven html

---
+++ modules/garden-sixty-seven.php
@@ -0,0 +1,34 @@
+<office-poster>
+  <h3 class="every-time">every time</h3>
+
+  <h3 class="you-are">you are</h3>
+  
+  <h3 class="trying-something-new">
+    <span class="trying">trying</span> 
+    <span class="something">something</span> 
+    <span class="new">new</span>
+      
+  </h3>
+  
+  <section class="inner-voice">
+    <div class="inner-mantra">
+      <h4>keep up the good work</h4>
+      <h4>you're doing your best</h4>
+    </div>
+    
+    <p>the process will most likely be difficult, challenging, and not so enjoyable</p>
+  </section>
+  
+  <section class="right-path">
+    <p>that is when you should tell yourself you're on the right path</p>
+  </section>
+  
+  <section class="reward">
+    <p>don't associate the reward with ultimate happiness, instead, pat yourself on the back while you're still trying</p>
+    
+    <ul class="mantra-list">
+      <li>never stop trying</li>
+      <li>never give up</li>
+    </ul>
+  </section>
+</office-poster>
