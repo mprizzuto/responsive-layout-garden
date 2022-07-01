392edd83a69f4769a4420ecb6bb5462034bd8659
Marco R <mprizzuto88@gmail.com>
2022-07-01 00:02:30 -0400

    add html for garden 74

--- modules/garden-seventy-four.php
+++ modules/garden-seventy-four.php
@@ -1,1 +1,26 @@
-garden-seventy-four.php
+<beauty-school>
+	<header class="beauty-school-header">
+		<h3 class="beauty-school-title">Moon's beauty school</h3>
+
+		<p><q>the best beauty school ever!</q></p>
+
+		<p class="beauty-standard">at the forefront of <em>korean beauty</em> standards</p>
+
+		<figure class="beauty-school-founder">
+			<picture class="founder-portrait">
+				<img src="https://womenintheworld.org/wp-content/uploads/2022/01/Han-Ye-Seul.jpg" alt="a korean woman with long hair">
+			</picture>
+
+			<figcaption>Han-Ye-Seul, our founder</figcaption>
+		</figure>
+	</header>
+
+	<section class="beauty-school-about">
+		<h3 class="beauty-bio-title">about beauty schools</h3>
+
+		<svg class="beauty-school-logo" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+		  <circle cx="50" cy="50" r="50"/>
+		</svg>
+
+	</section>
+</beauty-school>
