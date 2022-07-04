5435f0d14a3b9e76e72195e6a8a51bf675128fb7
Marco R <mprizzuto88@gmail.com>
2022-07-03 23:49:00 -0400

    add garden 77 html

--- modules/garden-seventy-seven.php
+++ modules/garden-seventy-seven.php
@@ -1,2 +1,30 @@
-<architect-poster>
+<architect-wall>
+	<architect-poster>
+	<header class="architect-poster-header">
+		<div class="architect-cta">
+			<h3 class="architect-poster-heading">discover</h3>
+		</div>
+
+		<h3 class="architect-name">le 
+			<span class="architect-surname">corbusier</span>
+
+		</h3>
+	</header>
+
+	<div class="architect-thoughts">
+		<p>to drape, to trace the lines, handle the volumes, organize the surfaces</p>
+
+		<p>all this means to look, deconstruct and innovate from the ground up</p>
+		<!-- <h3 class="architect-name">le 
+			<span class="architect-surname">corbusier</span>
+		</h3> -->
+	</div>
+
+	<!-- <picture class="architect-portrait">
+		<img src="https://www.nemolighting.com/wp-content/uploads/2019/07/LeCorbusier.jpg" alt="a man with glasses pointing on a paper">
+	</picture> -->
 </architect-poster>
+<picture class="architect-portrait">
+		<img src="https://www.nemolighting.com/wp-content/uploads/2019/07/LeCorbusier.jpg" alt="a man with glasses pointing on a paper">
+	</picture>
+</architect-wall>
