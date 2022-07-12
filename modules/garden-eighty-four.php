de7c50c29dae46f4363f378e79dfdb06b04539cc
Marco R <mprizzuto88@gmail.com>
2022-07-12 00:12:21 -0400

    add HTML with garden 84

---
+++ modules/garden-eighty-four.php
@@ -0,0 +1,50 @@
+<speaker>
+  <sidebar>
+    <h3 class="sidebar-title">JULY 30 | 9AM PT | Los Angeles, CA</h3>
+  </sidebar>
+
+  <dome>
+    <svg class="speaker-badge" width="100%" height="100%" viewBox="0 0 400 400" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+      <g transform="matrix(1,0,0,1,-70,-20)">
+          <g transform="matrix(1.05263,0,0,1.08108,-203.684,-142.162)">
+              <ellipse cx="450" cy="335" rx="190" ry="185" style="fill:rgb(255,129,0);"/>
+          </g>
+          <g transform="matrix(0.458135,-0.888883,0.888883,0.458135,-160.825,441.233)">
+              <g transform="matrix(0.830626,-0.55683,0.55683,0.830626,-49.0652,198.762)">
+                  <text x="302.191px" y="180.034px" style="font-family:'Neuton-Regular', 'Neuton';font-size:72px;stroke:black;stroke-width:1px;">S</text>
+              </g>
+              <g transform="matrix(0.948872,-0.315661,0.315661,0.948872,-34.5402,111.62)">
+                  <text x="327.299px" y="162.435px" style="font-family:'Neuton-Regular', 'Neuton';font-size:72px;stroke:black;stroke-width:1px;">P</text>
+              </g>
+              <g transform="matrix(0.999593,-0.0285323,0.0285323,0.999593,-4.15078,10.3653)">
+                  <text x="361.132px" y="150.629px" style="font-family:'Neuton-Regular', 'Neuton';font-size:72px;stroke:black;stroke-width:1px;">E</text>
+              </g>
+              <g transform="matrix(0.960603,0.277924,-0.277924,0.960603,57.139,-105.108)">
+                  <text x="399.311px" y="148.988px" style="font-family:'Neuton-Regular', 'Neuton';font-size:72px;stroke:black;stroke-width:1px;">A</text>
+              </g>
+              <g transform="matrix(0.793526,0.608536,-0.608536,0.793526,189.952,-236.082)">
+                  <text x="442.876px" y="161.88px" style="font-family:'Neuton-Regular', 'Neuton';font-size:72px;stroke:black;stroke-width:1px;">K</text>
+              </g>
+              <g transform="matrix(0.559943,0.828531,-0.828531,0.559943,366.097,-312.572)">
+                  <text x="477.301px" y="188.354px" style="font-family:'Neuton-Regular', 'Neuton';font-size:72px;stroke:black;stroke-width:1px;">E</text>
+              </g>
+              <g transform="matrix(0.267455,0.96357,-0.96357,0.267455,577.438,-320.305)">
+                  <text x="499.38px" y="219.621px" style="font-family:'Neuton-Regular', 'Neuton';font-size:72px;stroke:black;stroke-width:1px;">R</text>
+              </g>
+          </g>
+      </g>
+    </svg>
+  </dome>
+
+  <figure class="speaker-figure">
+    <picture class="speaker-portrait">
+      <img src="https://peprojects.dev/images/portrait.jpg" alt="">
+    </picture>
+
+    <figcaption><em class="speaker-name">davina ferreira</em> <span>founder + ceo</span> <span>alegria media and publishing</span></figcaption>
+  </figure>
+
+  <sidebar>
+    <h3 class="sidebar-title">#WEALLGROW Las Founders</h3>
+  </sidebar>
+</speaker>
