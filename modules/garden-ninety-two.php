aecd50af1f55ce1868569ddec3d14767f2874181
Marco R <mprizzuto88@gmail.com>
2022-07-18 17:51:14 -0400

    add garden 92 html

---
+++ modules/garden-ninety-two.php
@@ -0,0 +1,28 @@
+<therapist-card>
+  <section class="myth">
+    <h3 class="therapist-title">myth</h3>
+    
+    <p>Personality weakness or character flaws cause mental health problems. People with mental health problems can snap out of it.</p>
+  </section>
+  
+  <section class="fact">
+    <h3 class="therapist-title">fact
+      <svg class="therapist-dot" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="50" cy="50" r="50"/>
+      </svg>
+    </h3>
+    
+    <p>Mental health problems have nothing to do with being lazy or weak and many people need help to get better. Many factors contribute to mental health problems</p>
+    
+    <ul class="mental-illness-causes">
+      <li>Biological factors, such as genes, physical illness, injury, or brain chemistry.</li>
+      <li>Life experiences, such as trauma or a history of abuse.</li>
+      <li>Family history of mental health problems.</li>
+    </ul>
+  </section>
+  
+  <aside class="therapist-info">
+    <h3 class="therapist-title"><a href="#" taret="_external">pinkertonpsychotherapy.com</a> <span class="title-accent"></span></h3>
+  </aside>
+  
+</therapist-card>
