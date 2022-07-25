c54f9310f060140e64db3da017dba550a2889178
Marco R <mprizzuto88@gmail.com>
2022-07-25 00:00:23 -0400

    add garden 95 html

---
+++ modules/garden-ninety-five.php
@@ -0,0 +1,37 @@
+garden-ninety-five.php
+<sticky-notes>
+<sticky-note class="sticky-note-one">
+  <note>
+    <p>Taking a break isn't selfish, it's self-care</p>
+  </note>
+</sticky-note>
+
+<sticky-note class="sticky-note-two">
+  <note>
+    <p>IT's okay To have hard days. It doesn't mean you're failing</p>
+  </note>
+</sticky-note>
+
+<sticky-note class="sticky-note-three">
+  <note>
+    <p>You don'T have to face your battles alone</p>
+  </note>
+</sticky-note>
+
+<sticky-note class="sticky-note-four">
+  <note>
+    <p>You are worthy even when you don't feel like it</p>
+  </note>
+</sticky-note>
+
+<sticky-note class="sticky-note-five">
+  <note>
+    <p>Other's opinions of you are not your responsibility</p>
+  </note>
+</sticky-note>
+
+<sticky-note class="sticky-note-six">
+  <note>
+    <p>Don't make life-changing decisions with momentary feelings</p>
+  </note>
+</sticky-note>
