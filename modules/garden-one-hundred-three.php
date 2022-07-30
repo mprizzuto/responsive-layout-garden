74171c2682dd7d8d2896a6e66375b4b28c0155b6
Marco R <mprizzuto88@gmail.com>
2022-07-30 19:46:14 -0400

    add garden 103 html

--- modules/garden-one-hundred-three.php
+++ modules/garden-one-hundred-three.php
@@ -1,10 +1,74 @@
 <human-emotions>
 	<header class="human-emotions-header">
-		
+		<h3 class="human-emotions-title">the seven basic emotions and their universal expressions</h3>
 	</header>
 
+	<section class="human-expressions">
+		<figure class="emotion">
+			<picture class="person-emotes">
+				<img src="https://peprojects.dev/images/portrait.jpg" alt="">
+			</picture>
+
+			<figcaption>joy</figcaption>
+		</figure>
+
+		<emotions-grouped>
+			<figure class="emotion">
+				<picture class="person-emotes">
+					<img src="https://peprojects.dev/images/portrait.jpg" alt="">
+				</picture>
+
+				<figcaption>surprise</figcaption>
+			</figure>
+
+			<figure class="emotion">
+				<picture class="person-emotes">
+					<img src="https://peprojects.dev/images/portrait.jpg" alt="">
+				</picture>
+
+				<figcaption>contempt</figcaption>
+			</figure>
+		</emotions-grouped>
+
+		<figure class="emotion">
+			<picture class="person-emotes">
+				<img src="https://peprojects.dev/images/portrait.jpg" alt="">
+			</picture>
+
+			<figcaption>joy</figcaption>
+		</figure>
+
+		<emotions-grouped>
+			<figure class="emotion">
+				<picture class="person-emotes">
+					<img src="https://peprojects.dev/images/portrait.jpg" alt="">
+				</picture>
+
+				<figcaption>surprise</figcaption>
+			</figure>
+
+			<figure class="emotion">
+				<picture class="person-emotes">
+					<img src="https://peprojects.dev/images/portrait.jpg" alt="">
+				</picture>
+
+				<figcaption>contempt</figcaption>
+			</figure>
+		</emotions-grouped>
+
+		<figure class="emotion">
+			<picture class="person-emotes">
+				<img src="https://peprojects.dev/images/portrait.jpg" alt="">
+			</picture>
+
+			<figcaption>contempt</figcaption>
+		</figure>
+	</section>
+
 	<footer class="human-emotions-footer">
-		
+		<picture class="consultant-logo">
+			<img src="" alt="">
+		</picture>
 	</footer>
 
 
