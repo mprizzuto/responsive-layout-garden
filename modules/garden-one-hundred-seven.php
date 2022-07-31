0b7963e3a0b23fcc1279d87410f3c074b2a89bd6
Marco R <mprizzuto88@gmail.com>
2022-07-31 02:58:48 -0400

    add garden 107 html

---
+++ modules/garden-one-hundred-seven.php
@@ -0,0 +1,52 @@
+<school-election>
+	<h3 class="election-poster-title">
+		&#9733;
+		<span class="congrats">congratulations!</span>
+		<span class="congrats">congratulations!</span>&#9733;
+	</h3>
+
+	<section class="elects">
+		<h3 class="election-poster-title">
+			&#9733;
+			<span class="congrats">congratulations!</span>
+			<span class="congrats">congratulations!</span>&#9733;
+		</h3>
+
+		<chair-elect>
+			<h3 class="school-election-title">
+				<span>aasl</span>
+				<span>independent</span>
+				<span>schools</span>
+				<span>section</span>
+				<span>chair-elect</span>
+
+				<strong class="elect-name"><span>Hannah</span></strong>
+				<strong class="elect-name"><span>Byrd Little</span></strong>
+			</h3>
+		</chair-elect>
+
+		<chair-elect>
+			<h3 class="school-election-title">
+				<span>aasl</span>
+				<span>supervisors</span>
+				<span>section</span>
+				<span>chair-elect</span>
+
+				<strong class="elect-name"><span>lindsey</span></strong>
+				<strong class="elect-name"><span>kimery</span></strong>
+			</h3>
+		</chair-elect>
+
+		<h3 class="election-poster-title">
+			&#9733;
+			<span class="congrats">congratulations!</span>
+			<span class="congrats">congratulations!</span>&#9733;
+		</h3>
+	</section>
+
+	<h3 class="election-poster-title">
+		&#9733;
+		<span class="congrats">congratulations!</span>
+		<span class="congrats">congratulations!</span>&#9733;
+	</h3>
+</school-election>
