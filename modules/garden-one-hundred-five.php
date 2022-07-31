7b97d5405d3c8a215aff16a6600729c335684e1e
Marco R <mprizzuto88@gmail.com>
2022-07-31 00:20:28 -0400

    add garden 105 html

---
+++ modules/garden-one-hundred-five.php
@@ -0,0 +1,10 @@
+<temperature-card>
+  <h3 class="thermostat-title">drop the</h3>
+
+  <div class="thermostat-product">
+    <h3 class="thermostat-title">thermostat</h3>
+    <picture class="thermostat-pic">
+      <img src="https://peprojects.dev/images/square.jpg" alt="">
+    </picture>
+  </div>
+</temperature-card>
