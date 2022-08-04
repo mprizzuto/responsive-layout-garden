1b16cc974796e6b0d20c2a388dc57dd25f467293
Marco R <mprizzuto88@gmail.com>
2022-08-03 22:10:07 -0400

    add garden 109 html

---
+++ modules/garden-one-hundred-nine.php
@@ -0,0 +1,20 @@
+<super-creative>
+  <figure class="creative-figure">
+    <h3 class="creative-title">liv buchanan</h3>
+    
+    <picture class="creative-photo">
+      <img src="https://peprojects.dev/images/portrait.jpg" alt="">
+    </picture>
+    
+    <figcaption>the little op shop photograpaher</figcaption>
+    
+    <h3 class="super-creative-title">
+<!--           <span class="super-creative"> -->
+        <span>super</span>
+        <span>creative</span>
+<!--           </span> -->
+<!--           <span class="episode-num">episode 3</span> -->
+    </h3>
+    <span class="episode-num">episode 3</span>
+  </figure>
+</super-creative>
