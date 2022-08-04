12fcbab070314bb1f9b4238d751d1bc9619f276c
Marco R <mprizzuto88@gmail.com>
2022-08-03 22:25:40 -0400

    add garden 110 html

---
+++ modules/garden-one-hundred-ten.php
@@ -0,0 +1,18 @@
+<skincare-clinic>
+  <section class="client-photos">
+    <picture class="client-photo">
+      <img src="https://peprojects.dev/images/portrait.jpg" alt="">
+    </picture>
+
+    <p class="skin-care-label">natural skincare clinic</p>
+    
+    <photo-frame>
+      <h3 class="office-pet-greeting">
+        <span>happy</span>
+        <span>birthday</span>
+        <span>clover</span>
+      </h3>
+      <tape></tape>
+    </photo-frame>
+  </section>
+</skincare-clinic>
