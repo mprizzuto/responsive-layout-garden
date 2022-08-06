b0740d9a8c2ba75fe2f0c671231a56f91bafb2b7
Marco R <mprizzuto88@gmail.com>
2022-08-06 19:26:17 -0400

    add garden 113 html

---
+++ modules/garden-one-hundred-thirteen.php
@@ -0,0 +1,14 @@
+<geometric-art>
+  <section class="geometric-one"></section>
+
+
+  <section class="web-agency-details">
+    <h3 class="web-agency-title">this is the title</h3>
+    
+    <p class="details-paragraph">this is the pararaph that supports he above title. we dont know what itll be , but it will be descriptive and concise</p>
+  </section>
+  
+  <section class="geometric-two"></section>
+  
+
+</geometric-art>
