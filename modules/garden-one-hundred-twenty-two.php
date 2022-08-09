31f78a748360e4ba6a30b70671cd16dd860fdc61
Marco R <mprizzuto88@gmail.com>
2022-08-08 22:16:55 -0400

    add garden 122 html

---
+++ modules/garden-one-hundred-twenty-two.php
@@ -0,0 +1,41 @@
+<self-help-poster>
+	<h3 class="self-help-poster-title">
+		<span class="self-help-word">the</span>
+		<span class="self-help-accent"></span>
+	</h3>
+
+	<h3 class="self-help-poster-title">
+		<span class="self-help-word">less</span>
+		<span class="self-help-accent"></span>
+	</h3>
+
+	<h3 class="self-help-poster-title">
+		<span class="self-help-word">we</span>
+		<span class="self-help-accent"></span>
+	</h3>
+
+	<h3 class="self-help-poster-title">
+		<span class="self-help-word">compare</span>
+		<span class="self-help-accent"></span>
+	</h3>
+
+	<h3 class="self-help-poster-title">
+		<span class="self-help-word">the</span>
+		<span class="self-help-accent"></span>
+	</h3>
+
+	<h3 class="self-help-poster-title">
+		<span class="self-help-word">more</span>
+		<span class="self-help-accent"></span>
+	</h3>
+
+	<h3 class="self-help-poster-title">
+		<span class="self-help-word">we</span>
+		<span class="self-help-accent"></span>
+	</h3>
+
+	<h3 class="self-help-poster-title">
+		<span class="self-help-word">become</span>
+		<span class="self-help-accent"></span>
+	</h3>
+</self-help-poster>
