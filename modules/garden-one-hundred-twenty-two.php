0a4e2d86756536621370b8107922c4787da4d7f6
Marco R <mprizzuto88@gmail.com>
2022-08-08 23:00:07 -0400

    add garden 122 html

--- modules/garden-one-hundred-twenty-two.php
+++ modules/garden-one-hundred-twenty-two.php
@@ -6,36 +6,36 @@
 
 	<h3 class="self-help-poster-title">
 		<span class="self-help-word">less</span>
-		<span class="self-help-accent"></span>
+		<span class="self-help-accent self-help-accent-two"></span>
 	</h3>
 
 	<h3 class="self-help-poster-title">
 		<span class="self-help-word">we</span>
-		<span class="self-help-accent"></span>
+		<span class="self-help-accent self-help-accent-three"></span>
 	</h3>
 
 	<h3 class="self-help-poster-title">
 		<span class="self-help-word">compare</span>
-		<span class="self-help-accent"></span>
+		<span class="self-help-accent self-help-accent-four"></span>
 	</h3>
 
 	<h3 class="self-help-poster-title">
 		<span class="self-help-word">the</span>
-		<span class="self-help-accent"></span>
+		<span class="self-help-accent self-help-accent-four"></span>
 	</h3>
 
 	<h3 class="self-help-poster-title">
 		<span class="self-help-word">more</span>
-		<span class="self-help-accent"></span>
+		<span class="self-help-accent self-help-accent-five"></span>
 	</h3>
 
 	<h3 class="self-help-poster-title">
 		<span class="self-help-word">we</span>
-		<span class="self-help-accent"></span>
+		<span class="self-help-accent self-help-accent-six"></span>
 	</h3>
 
 	<h3 class="self-help-poster-title">
 		<span class="self-help-word">become</span>
-		<span class="self-help-accent"></span>
+		<span class="self-help-accent self-help-accent-seven"></span>
 	</h3>
 </self-help-poster>
