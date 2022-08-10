88fd31bbeaa5a559636df5c3f87e1b3958d15ac9
Marco R <mprizzuto88@gmail.com>
2022-08-10 17:27:47 -0400

    add garden 126 html

---
+++ modules/garden-one-hundred-twenty-six.php
@@ -0,0 +1,18 @@
+<taxi-cab>
+  <h3 class="taxi-cab-name">
+    <span class="nyc-taxi-location">nyc</span>
+    
+    <svg class="nyc-taxi-logo" width="100%" height="100%" viewBox="0 0 500 500" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+      <g transform="matrix(1,0,0,1,-130,-50)">
+          <g transform="matrix(1.25,0,0,1.2166,-170,-144.655)">
+              <ellipse cx="440" cy="365.491" rx="200" ry="205.491"/>
+          </g>
+          <g transform="matrix(1,0,0,1,250.567,178.4)">
+              <text x="46.037px" y="221.6px" style="font-family:'Neuton-Regular', 'Neuton';font-size:288px;fill:white;">T</text>
+          </g>
+      </g>
+    </svg>
+    
+    <span class="nyc-taxi">boro</span>
+  </h3>
+</taxi-cab>
