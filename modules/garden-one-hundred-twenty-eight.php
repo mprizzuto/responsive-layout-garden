4bd565f9a47a4e14ae9b6394a20fd2cd3410cdbf
Marco R <mprizzuto88@gmail.com>
2022-08-13 03:43:02 -0400

    add garden 128 html

---
+++ modules/garden-one-hundred-twenty-eight.php
@@ -0,0 +1,13 @@
+<cbt-thought-diary>
+	<section class="bw-thinking">
+		<h3 class="bw-thinking-title">black or white thinking</h3>
+
+		<p class="bw-thought"><q>I always fail</q></p>
+	</section>
+
+	<section class="cognitive-distortion">
+		<h3 class="cognitive-distortion-title">Cognitive distortion</h3>
+
+		<p class="bw-thought"><q>nobody likes me</q></p>
+	</section>
+</cbt-thought-diary>
