d88d0602c22c564d64f00e8d3353039317441397
Marco R <mprizzuto88@gmail.com>
2022-08-15 02:45:13 -0400

    add garden 130 html

---
+++ modules/garden-one-hundred-thirty.php
@@ -0,0 +1,109 @@
+<learn-how-to-learn>
+	<h3 class="how-to-learn-title">learn how to learn</h3>
+
+	<how-to-learn>
+		<!-- <ul class="learning-strategy-list">
+			<li>
+				<span></span>
+			</li>
+
+			<li>
+				<span></span>
+			</li>
+
+			<li>
+				<span></span>
+			</li>
+
+			<li>
+				<span></span>
+			</li>
+
+			<li>
+				<span></span>
+			</li>
+
+			<li>
+				<span></span>
+			</li>
+		</ul> -->
+
+		<ul class="learning-strategies">
+			<li>
+				<span class="learning-strategy">create</span>
+			</li>
+
+			<li>
+				<span class="learning-strategy">evaluate</span>
+			</li>
+
+			<li>
+				<span class="learning-strategy">analyze</span>
+			</li>
+
+			<li>
+				<span class="learning-strategy">apply</span>
+			</li>
+
+			<li>
+				<span class="learning-strategy">understand</span>
+			</li>
+
+			<li>
+				<span class="learning-strategy">remember</span>
+			</li>
+		</ul>
+
+		<ul class="learning-methods-explained">
+			<li>
+				<learning-method>
+					<h4 class="learning-method-title">the Sq3r learning method</h4>
+
+					<p class="learning-method-described">is a reading
+information within their textbook. SQ3R (or SQRRR) is an acronym that stands for the five steps
+or the reading</p>
+				</learning-method>
+			</li>
+
+			<li>
+				<learning-method>
+					<h4 class="learning-method-title"></h4>
+
+					<p class="learning-method-described"></p>
+				</learning-method>
+			</li>
+
+			<li>
+				<learning-method>
+					<h4 class="learning-method-title"></h4>
+
+					<p class="learning-method-described"></p>
+				</learning-method>
+			</li>
+
+			<li>
+				<learning-method>
+					<h4 class="learning-method-title"></h4>
+
+					<p class="learning-method-described"></p>
+				</learning-method>
+			</li>
+
+			<li>
+				<learning-method>
+					<h4 class="learning-method-title"></h4>
+
+					<p class="learning-method-described"></p>
+				</learning-method>
+			</li>
+
+			<li>
+				<learning-method>
+					<h4 class="learning-method-title"></h4>
+
+					<p class="learning-method-described"></p>
+				</learning-method>
+			</li>
+		</ul>
+	</how-to-learn>
+</learn-how-to-learn>
