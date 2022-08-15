dc2d8b7e1fde7cd2b44a0ac8c4eb3b1a993ef5d4
Marco R <mprizzuto88@gmail.com>
2022-08-15 17:13:31 -0400

    format garden 130 html

--- modules/garden-one-hundred-thirty.php
+++ modules/garden-one-hundred-thirty.php
@@ -2,32 +2,6 @@
 	<h3 class="how-to-learn-title">learn how to learn</h3>
 
 	<how-to-learn>
-		<!-- <ul class="learning-strategy-list">
-			<li>
-				<span></span>
-			</li>
-
-			<li>
-				<span></span>
-			</li>
-
-			<li>
-				<span></span>
-			</li>
-
-			<li>
-				<span></span>
-			</li>
-
-			<li>
-				<span></span>
-			</li>
-
-			<li>
-				<span></span>
-			</li>
-		</ul> -->
-
 		<ul class="learning-strategies">
 			<li>
 				<span class="learning-strategy">create</span>
@@ -67,41 +41,41 @@ or the reading</p>
 
 			<li>
 				<learning-method>
-					<h4 class="learning-method-title"></h4>
+					<h4 class="learning-method-title">retrieval practice</h4>
 
-					<p class="learning-method-described"></p>
+					<p class="learning-method-described">is based on the concept of remembering at a later time. recallingan answer to a quesiton improves learning more than looking for the answer in your textbook. <br> And remembering and writing down the answer to a flashcard is a lot more effective than thinking you know the answer and flipping the card over early</p>
 				</learning-method>
 			</li>
 
 			<li>
 				<learning-method>
-					<h4 class="learning-method-title"></h4>
+					<h4 class="learning-method-title">spaced practice</h4>
 
-					<p class="learning-method-described"></p>
+					<p class="learning-method-described">encourages students to study over a longer period of time instead of cramming the night before, when our brains almost forget something, thev work harder to recall tht information. <br> spacing out yout studying allows your mind to conenctions between ideas and build upon that knowledge that can easily be recalled later</p>
 				</learning-method>
 			</li>
 
 			<li>
 				<learning-method>
-					<h4 class="learning-method-title"></h4>
+					<h4 class="learning-method-title">The PQ4R method</h4>
 
-					<p class="learning-method-described"></p>
+					<p class="learning-method-described">takes an active approach to learning that improves memorization and understanding of the topic. similar to the Sq3R method above, PQ4R is an acronym that stands for the six steps in the process</p>
 				</learning-method>
 			</li>
 
 			<li>
 				<learning-method>
-					<h4 class="learning-method-title"></h4>
+					<h4 class="learning-method-title">The Leitner System</h4>
 
-					<p class="learning-method-described"></p>
+					<p class="learning-method-described">is a learning technique based on flashcards. Ideally you keep your cards in several different boxes to track when you need to study each set. <br>Every card starts in Box 1. If you get a card right, you move it to the next box. If you get a card wrong, you either move it down a box or keep it in Box 1 It its areadvt</p>
 				</learning-method>
 			</li>
 
 			<li>
 				<learning-method>
-					<h4 class="learning-method-title"></h4>
+					<h4 class="learning-method-title">A mind map</h4>
 
-					<p class="learning-method-described"></p>
+					<p class="learning-method-described">The structure of a mind map is related to how our brains store and retrive information. Mindmapping your notes instead of just writing them down can improve your reading comprehension. <br> It also enables you to see the big picture by communicating the hierarchy and relationships betwene concepts and ideas</p>
 				</learning-method>
 			</li>
 		</ul>
