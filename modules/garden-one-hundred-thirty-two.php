9edb40948b22ec0d0364f9e6ca2adf2cd04baefb
Marco R <mprizzuto88@gmail.com>
2022-08-18 22:43:57 -0400

    add garden 132 html

---
+++ modules/garden-one-hundred-thirty-two.php
@@ -0,0 +1,52 @@
+<art-business-card>
+  <sunset>
+    <svg width="100%" height="100%" viewBox="0 0 1853 1336" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+      <g id="Artboard1" transform="matrix(1.65422,0,0,1.65473,132.338,198.568)">
+          <rect x="-80" y="-120" width="1120" height="807.34" style="fill:none;"/>
+          <g transform="matrix(0.604514,0,0,0.604327,9.94667,-76.342)">
+              <rect x="20" y="20" width="740" height="1080" style="fill:url(#_Linear1);"/>
+          </g>
+          <g transform="matrix(0.604514,0,0,0.604327,445.197,-76.342)">
+              <rect x="20" y="20" width="740" height="1080" style="fill:url(#_Linear2);"/>
+          </g>
+          <g transform="matrix(3.64292e-17,-0.604327,0.620852,3.81639e-17,68.2008,784.42)">
+              <path d="M1240,630C1240,425.655 1078.82,260 880,260C681.177,260 520,425.655 520,630L1240,630Z" style="fill:url(#_Linear3);"/>
+          </g>
+          <g transform="matrix(3.81639e-17,0.604327,-0.620852,4.85723e-17,850.475,-279.195)">
+              <path d="M1240,630C1240,425.655 1078.82,260 880,260C681.177,260 520,425.655 520,630L1240,630Z" style="fill:url(#_Linear4);"/>
+          </g>
+      </g>
+      <defs>
+          <linearGradient id="_Linear1" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(40,1120,-1120,40,271.208,27.7576)"><stop offset="0" style="stop-color:rgb(91,92,255);stop-opacity:1"/><stop offset="1" style="stop-color:rgb(52,0,237);stop-opacity:1"/></linearGradient>
+          <linearGradient id="_Linear2" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(-680,1040,-1040,-680,731.208,27.7576)"><stop offset="0" style="stop-color:rgb(120,0,255);stop-opacity:1"/><stop offset="1" style="stop-color:rgb(58,0,224);stop-opacity:1"/></linearGradient>
+          <linearGradient id="_Linear3" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(-500,19.4737,-20,-486.842,1196.58,715.505)"><stop offset="0" style="stop-color:rgb(255,162,0);stop-opacity:1"/><stop offset="1" style="stop-color:rgb(164,94,0);stop-opacity:1"/></linearGradient>
+          <linearGradient id="_Linear4" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(-480,-19.4737,20,-467.368,1163.42,505.547)"><stop offset="0" style="stop-color:rgb(255,162,0);stop-opacity:1"/><stop offset="1" style="stop-color:rgb(164,94,0);stop-opacity:1"/></linearGradient>
+      </defs>
+    </svg>
+  </sunset>
+  
+  <business-card>
+    <ul class="art-director-contact-methods">
+      <li class="art-director-email">
+        <a href="#">ginaMills.tossas@gmail.com</a>
+      </li>
+      
+       <li class="art-director-name">
+        <span>gina mills tosas</span>
+         <span>art direction & set design</span>
+      </li>
+      
+       <li class="art-director-website">
+        <a href="#">ginamillstossas.com</a>
+      </li>
+      
+      <li class="art-director-handle">
+        <a href="#">@ginamills</a>
+      </li>
+      
+      <li class="art-director-number">
+        <a href="#">+34 638 962 168</a>
+      </li>
+    </ul>
+  </business-card>
+</art-business-card>
