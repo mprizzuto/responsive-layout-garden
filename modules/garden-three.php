82e31b6de45b8619a4e3f198def036d8c6ae6fcb
Marco R <mprizzuto88@gmail.com>
2022-08-21 01:49:47 -0400

    encapsulate HTML within custom element

--- modules/garden-three.php
+++ modules/garden-three.php
@@ -1,4 +1,4 @@
-  <h3 class="heading-three" aria-hidden="true">ftv</h3>
+<jennifer-lee-lacks>
   <div class="actress-highlight">
     <h3 class="heasding-three">Jennifer Lee Laks</h3>
     <picture class="theater-actress">
@@ -6,24 +6,25 @@
     </picture>
     
     <p>Jennifer is a graduate of the William Esper Studio in NY. LA Theatre: Nicole in Homeward LA 2019 (Moveable Theatre &Theatre 40), Jenn/Carla in 26 Pebbles (Theatre 40), Sara in The
-Super Sabado Show (Hero Theatre), Gwen in The Liar's Punishment (2017 Hollywood Fringe Festival), April in April, May</p>
+  Super Sabado Show (Hero Theatre), Gwen in The Liar's Punishment (2017 Hollywood Fringe Festival), April in April, May</p>
     
     <p>June (Theatre 40), Madeline in The Armadillo Necktie (The
-Group Rep), Nancy in Another Washington Affair (The Avery
-Schreiber Playhouse), Suzette in Don't Dress for Dinner
-(The Group Rep), Justine in The Love List (Theatre 40),
-Elaine in Night Watch (Theatre 40), Jen/Rachel in 7 Stories
-(Theatre 40). TV / Film: Crown Lake, Shameless, Astrid
-Clover (Pilot '17), Cry Wolfe, American Playhouse, Young
-Love, ReWrite, Elvis Lives!, Beverly H., The Goat Shearing.
-Thank you to my husband, my mother, Denise, Sally, Ron,
-the incredible cast and crew, and María Irene Fornés for her
-immortal energy.</p>
+  Group Rep), Nancy in Another Washington Affair (The Avery
+  Schreiber Playhouse), Suzette in Don't Dress for Dinner
+  (The Group Rep), Justine in The Love List (Theatre 40),
+  Elaine in Night Watch (Theatre 40), Jen/Rachel in 7 Stories
+  (Theatre 40). TV / Film: Crown Lake, Shameless, Astrid
+  Clover (Pilot '17), Cry Wolfe, American Playhouse, Young
+  Love, ReWrite, Elvis Lives!, Beverly H., The Goat Shearing.
+  Thank you to my husband, my mother, Denise, Sally, Ron,
+  the incredible cast and crew, and María Irene Fornés for her
+  immortal energy.</p>
   </div>
-  
+
   <aside class="show-dates">
     <h4 class="heading-four">dates</h4>
     
     <p>Box Office 310-477-2055 EXT. 2 Box Office Hours finurse lom • kemar curan FriSat- fem - Rom Regular Performances Every Friday & Saturday at 8pm Wednesdays at 8pm Tix for $10 Fridav Bool. 16 11
-Proviows SiO PosiesnowDiscussions Aug. 16 & Sopt. 13 & 15 Wine Night Friday Aug. 16 & Seps 20 lenov comomentary wine any snacks an minol with the cast after the show College Night Sept 18 ES10 wth vale studen ll luso oromo cogn CollEct Includes a pre-partormance</p>
-</aside>
+  Proviows SiO PosiesnowDiscussions Aug. 16 & Sopt. 13 & 15 Wine Night Friday Aug. 16 & Seps 20 lenov comomentary wine any snacks an minol with the cast after the show College Night Sept 18 ES10 wth vale studen ll luso oromo cogn CollEct Includes a pre-partormance</p>
+  </aside>
+</jennifer-lee-lacks>
