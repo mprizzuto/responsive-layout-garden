bf1451ed3d091bf094c583433ab1c143ebc7559a
Marco R <mprizzuto88@gmail.com>
2022-08-21 03:00:51 -0400

    encapsulate HTML in custom element

--- modules/garden-four.php
+++ modules/garden-four.php
@@ -1,10 +1,12 @@
-<h3 class="attention-voice">dentists are cool</h3>
+<dentistry-tools>
+  <h3 class="dentist-recommends">Tools Dentists recommend</h3>
 
-<ul class="dentist-tools">
-  <li aria-label="a tongue cleaner"></li>
-  <li aria-label="a plaque cleaner"></li>
-  <li aria-label="a dental mirror"></li>
-  <li aria-label="floss"></li>
-  <li aria-label="toothpaste"></li>
-  <li aria-label="toothbrush"></li>
-</ul>
+  <ul class="dentist-tools">
+    <li aria-label="a tongue cleaner"></li>
+    <li aria-label="a plaque cleaner"></li>
+    <li aria-label="a dental mirror"></li>
+    <li aria-label="floss"></li>
+    <li aria-label="toothpaste"></li>
+    <li aria-label="toothbrush"></li>
+  </ul>
+</dentistry-tools>
