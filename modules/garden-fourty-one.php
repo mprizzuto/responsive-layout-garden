05d4950b6de2f4bb8eb741c9c4f585495f40302e
Marco R <mprizzuto88@gmail.com>
2022-12-10 19:43:48 -0500

    add blockquote content

--- modules/garden-fourty-one.php
+++ modules/garden-fourty-one.php
@@ -25,3 +25,15 @@
     <figcaption>celeb couple 3</figcaption>
   </figure>
 </celebrity-photos>
+
+<section class="celeb-style">
+
+  <blockquote class="celeb-article-snippet" cite="https://www.elle.com.au/fashion/brad-pitt-girlfriends-20698">
+    <p>In recent years, Brad Pitt's love life has been confined to being both Angelina Jolie's husband, or Angelina Jolie's ex-husband.</p>
+
+    <p>But before he met his now-ex-wife on the set of Mr & Mrs Smith (prompting the now-infamous Jen Vs Ange-gate) Pitt's love life was vast and varied, even featuring a few familiar faces you've probably forgotten about.</p>
+
+    <p>And during his active dating days, Pitt became well-known for dressing like his latest love interest. No, really.</p>
+  </blockquote>
+  
+</section>
