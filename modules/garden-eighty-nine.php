638d9bd49c44e1763769bc0356c152225b93486f
Marco R <mprizzuto88@gmail.com>
2022-12-16 23:31:17 -0500

    make images local garden

--- modules/garden-eighty-nine.php
+++ modules/garden-eighty-nine.php
@@ -2,23 +2,23 @@
   <h3 class="pandemic-news-title">the pandemic's toll</h3>
   <section class="pandemic-victims">
     <picture class="victim-photo">
-      <img src="https://peprojects.dev/images/square.jpg" alt="">
+      <img src="./images/garden-photos/garden-eighty-nine/health-care-one.jpg" alt="a woman with a mask">
     </picture>
     
     <picture class="victim-photo">
-      <img src="https://peprojects.dev/images/square.jpg" alt="">
+      <img src="./images/garden-photos/garden-eighty-nine/health-care-two.jpg" alt="a man in glasses">
     </picture>
     
     <picture class="victim-photo">
-      <img src="https://peprojects.dev/images/square.jpg" alt="">
+      <img src="./images/garden-photos/garden-eighty-nine/health-care-three.jpg" alt="a man with a face shield">
     </picture>
     
     <picture class="victim-photo">
-      <img src="https://peprojects.dev/images/square.jpg" alt="">
+      <img src="./images/garden-photos/garden-eighty-nine/health-care-four.jpg" alt="a health care worker holds up a stethescope while smiling">
     </picture>
     
     <picture class="victim-photo">
-      <img src="https://peprojects.dev/images/square.jpg" alt="">
+      <img src="./images/garden-photos/garden-eighty-nine/health-care-five.jpg" alt="a man in a mask and green shirt">
     </picture>
     
   </section>
